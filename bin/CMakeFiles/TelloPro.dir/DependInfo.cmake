0773b53709262d4ca7f71bffc1a18fd8b0cd3cf3
bsh4724 <21700326@handong.edu>
2019-11-23 22:47:51 +0900

    Tello project

---
+++ bin/CMakeFiles/TelloPro.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/telloproject-bsh4724/src/flip.cpp" "/data/telloproject-bsh4724/bin/CMakeFiles/TelloPro.dir/src/flip.cpp.o"
+  "/data/telloproject-bsh4724/src/land.cpp" "/data/telloproject-bsh4724/bin/CMakeFiles/TelloPro.dir/src/land.cpp.o"
+  "/data/telloproject-bsh4724/src/python_interface.cpp" "/data/telloproject-bsh4724/bin/CMakeFiles/TelloPro.dir/src/python_interface.cpp.o"
+  "/data/telloproject-bsh4724/src/takeoff.cpp" "/data/telloproject-bsh4724/bin/CMakeFiles/TelloPro.dir/src/takeoff.cpp.o"
+  "/data/telloproject-bsh4724/src/up.cpp" "/data/telloproject-bsh4724/bin/CMakeFiles/TelloPro.dir/src/up.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/python3.6m"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
